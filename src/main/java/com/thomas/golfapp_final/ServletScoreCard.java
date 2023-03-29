23da15eb1b4d6bb15de88d0866d0ea0418a3fe04
darthcody91 <45984035+darthcody91@users.noreply.github.com>
2023-03-29 16:40:42 -0600

    Okay - this seems to be working with the servlet. just need to do the Sql stuff ---

--- src/main/java/com/thomas/golfapp_final/ServletScoreCard.java
+++ src/main/java/com/thomas/golfapp_final/ServletScoreCard.java
@@ -24,7 +24,8 @@ public class ServletScoreCard extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
-        out.println("<html><head></head><body>");
+        out.println("<html><head>"+"<link rel='stylesheet' href='golf_style.css'>"+"</head><body>");
+
         int hole1 = Integer.parseInt(request.getParameter("hole1"));
         int hole2 = Integer.parseInt(request.getParameter("hole2"));
         int hole3 = Integer.parseInt(request.getParameter("hole3"));
@@ -37,8 +38,16 @@ public class ServletScoreCard extends HttpServlet {
 
         int coursescore = Integer.parseInt(String.valueOf((hole1 + hole2 + hole3 + hole4 + hole5 + hole6 + hole7 + hole8 + hole9)));
 
+        out.println("<p> hello </p>");
+
         out.println("<h1>Course Score</h1>");
-        out.println("<p>Score: " + coursescore + "</p>");
+        out.println("<table>");
+        out.println("<tr><th>Hole 1</th><th>Hole 2</th><th>Hole 3</th><th>Hole 4</th><th>Hole 5</th><th>Hole 6</th><th>Hole 7</th><th>Hole 8</th><th>Hole 9</th><th> </th><th>Total</th</tr>");
+        out.println("<tr><td>" + hole1 + "</td><td>" + hole2 + "</td><td>" + hole3 + "</td><td>" + hole4 + "</td><td>" + hole5 + "</td><td>" + hole6 + "</td><td>" + hole7 + "</td><td>" + hole8 + "</td><td>" + hole9 + "</td><td>" + " " + "</td><td>" + coursescore + "</td></tr>");
+        out.println("</table>");
+
+
+
 
         out.println("</body></html>");
     }
